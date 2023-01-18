a8d32ce82660f727e02ba544e8be08cf55c057ce
MafazMalik <mafazmafs0206@gmail.com>
2023-01-18 12:51:35 +0530

    star

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'star';
+  starOne=['*','*', '*', '*'];
+  starTwo=['*'];
+  starThree=['*'];
+  starFour=['*'];
 }
